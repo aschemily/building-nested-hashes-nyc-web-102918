18cc4618cd4e22e5535be78f14fce71ae11eb7e2
Emily Asch <e.asch@me.com>
2018-10-16 17:11:35 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
+        :name => "Lord Montague"
         :age => "53"
         },
       :matriarch => {
@@ -13,8 +13,8 @@ def third_challenge
         },
       :hero => {
         #your key/value pairs here
-        :name => "Romeo",
-        :age => "15",
+        :name => "Romeo"
+        :age => "15"
         :status => "alive"
         },
       :hero_friends => []
